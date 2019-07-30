a44ac951a05fad03736ddb6c66dfe4f6c1837d51
xxxx591 <250173319@qq.com>
2019-07-30 17:23:17 +0800

    add

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,48 +7,42 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    msg: '长沙市'
   },
   //事件处理函数
   bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
+  onLoad: function() {
+    // this.init()
+    let res = wx.getStorageInfoSync();
+    console.log(res.keys)
+    let add = res.keys
+    if (add.indexOf('name') != -1 && add.indexOf('time') != -1 && add.indexOf('city') != -1){
+      wx.navigateTo({
+        url: '/pages/question/question',
       })
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  init() {
+    
+  },
+  submit() {
+    wx.navigateTo({
+      url: '/pages/question/question',
     })
-  }
+  },
+  getName(e) {
+    console.log(e.detail.value)
+    wx.setStorageSync('name', e.detail.value)
+  },
+  getTime(e) {
+    console.log(e.detail.value)
+    wx.setStorageSync('time', e.detail.value)
+  },
+  getCity(e) {
+    console.log(e.detail.value)
+    wx.setStorageSync('city', e.detail.value)
+  },
 })
--- pages/question/question.js
+++ pages/question/question.js
@@ -5,62 +5,159 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    activeName: '1',
+    name: '',
+    time: '',
+    city: '',
+    msg: "",
+    day:'',
+    obj:{}
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
+    this.setData({
+      name: wx.getStorageSync('name'),
+      time: wx.getStorageSync('time'),
+      city: wx.getStorageSync('city')
+    })
+    this.getDay()
+    this.init()
+  },
+  initAgain(){
+    this.init()
+  },
+  getDay(){
+    var date1 = new Date();
+    var date2 = new Date(this.data.time);
+    var date = (date1.getTime() - date2.getTime()) / (24 * 60 * 60 * 1000);
+    this.setData({
+      day: parseInt(date)
+    })
+  },
+  onChange(event) {
+    this.setData({
+      activeName: event.detail
+    });
+  },
+  init() {
+    wx.request({
+      url: 'https://api.tianapi.com/txapi/saylove/?key=f346e79d1626293d5056c1df555213d2',
+      method: 'GET',
+      success: res => {
+        console.log('res', res)
+        wx.request({
+          url: `https://www.tianqiapi.com/api/?version=v6&city=${this.data.city}`,
+          success: res1 => {
+            console.log('res1', res1)
+            let obj = {}
+            obj.tips0 = `亲爱的${this.data.name}`
+            obj.tips1 = `${res1.data.date}日 ${res1.data.update_time} ${res1.data.week}`
+            obj.tips2 = `我们在一起的第${this.data.day}天`
+            obj.tips3 = `元气满满的一天开始啦,要开心噢^_^`
+            obj.tips4 = `今日天气`
+            obj.tips5 = `${res1.data.air_tips}`
+            obj.tips6 = `今天：${res1.data.wea}`
+            obj.tips7 = `最高气温：${res1.data.tem2}° / ${res1.data.tem1}°`
+            obj.tips8 = `${res1.data.win} ${res1.data.win_speed}`
+            obj.tips9 = `空气质量：${res1.data.air_level}`
+            obj.tips10 = `每日一句：`
+            obj.tips11 = `${res.data.newslist[0].content}`
+            obj.tips12 = `————————最爱你的我`
+            let msg = ''
+            msg = `亲爱的${this.data.name}
+${res1.data.date}日 ${res1.data.update_time} ${res1.data.week}
+我们在一起的第${this.data.day}天
+
+元气满满的一天开始啦,要开心噢^_^
+
+今日天气
+${res1.data.air_tips}
+今天：${res1.data.wea}
+最高气温：${res1.data.tem2}° / ${res1.data.tem1}°
+${res1.data.win} ${res1.data.win_speed}
+空气质量：${res1.data.air_level}
+
+每日一句：
+${res.data.newslist[0].content}
+
+————————最爱你的我`
+            this.setData({
+              obj: obj,
+              msg: msg
+            })
+          }
+        })
+      }
+    })
 
   },
-
+  getVerificationCode() {
+    wx.setClipboardData({
+      data: this.data.msg,
+      success(res) {
+        wx.showToast({
+          title: '已复制',
+          icon: "success",
+          duration: 2000
+        })
+        wx.getClipboardData({
+          success(res) {
+            console.log(res.data) // data
+          }
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
